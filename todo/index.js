90d656e6405372183c14f4286e920fbb482628b2
nitesh tiwari <ntiwari@collegeboard.org>
2023-12-14 23:52:58 +0530

    fix: moved Todo Schema in the same file for vercel

--- todo/index.js
+++ todo/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const Todo = require("./todo-schema");
+const mongoose = require("mongoose");
+
+const todoSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamp: true }
+);
+
+const Todo = new mongoose.model("Todo", todoSchema);
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
